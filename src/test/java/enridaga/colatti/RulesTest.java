274bddb8c9acf9551327fbe8a7915f97d057f170
enridaga <enricodaga@gmail.com>
2016-05-29 20:11:48 +0100

    Unit tests for Rules extraction

--- src/test/java/enridaga/colatti/RulesTest.java
+++ src/test/java/enridaga/colatti/RulesTest.java
@@ -14,11 +14,11 @@ public class RulesTest {
 	public TestName name = new TestName();
 
 	private Rules test(Lattice lattice, Object[] inHead, Object[] inBody) {
-		l.info("testing: {} {}", inHead, inBody);
+		l.info("Testing: {} {}", inHead, inBody);
 		Rules rules = new Rules(lattice);
 		enridaga.colatti.Rules.Rule[] r = rules.rules(inHead, inBody);
 		for (enridaga.colatti.Rules.Rule x : r) {
-			l.info("{}", x);
+			l.trace("Generated {}", x);
 		}
 		return rules;
 	}
@@ -29,16 +29,17 @@ public class RulesTest {
 		InsertObject insert = new InsertObject(lattice);
 		insert.perform("A", 1, 2, 3, 4, 'a');
 		insert.perform("B", 2, 3, 4, 5, 'a');
-		insert.perform("C", 3, 4, 5, 6, 'b');
+		insert.perform("C", 3, 4, 5, 6, 'a','b','c');
 		insert.perform("D", 7, 8, 9, 10, 'b');
 		insert.perform("E", 11, 12, 13, 14, 15, 'c');
 		insert.perform("F", 10, 11, 12, 16, 17, 'd');
 		insert.perform("G", 16, 17, 18, 19, 20, 'c');
 		insert.perform("H", 1, 2, 3, 4, 5, 6, 'e');
 		insert.perform("I", 3, 4, 5, 6, 7, 8, 'f');
-		test(lattice, objects('a', 'b', 'c'), objects(2, 3, 7, 8, 9));
-		test(lattice, objects('a', 'b', 'c'), objects(2, 3));
-		test(lattice, objects('a', 'b', 'c'), objects(7, 8, 9));
+		Object[] vocabulary = objects('a', 'b', 'c', 'd', 'e', 'f');
+//		test(lattice, vocabulary, objects(2, 3, 7, 8, 9));
+		test(lattice, vocabulary, objects(2, 3));
+//		test(lattice, vocabulary, objects(7, 8, 9));
 	}
 
 	private static Object[] objects(Object... objects) {
