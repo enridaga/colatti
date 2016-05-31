ddaca1916e0c982bfe4a4c960588670a00cf95d5
enridaga <enricodaga@gmail.com>
2016-05-31 10:49:42 +0100

    Fixed references to interface (more)

--- src/main/java/enridaga/colatti/ConceptFactory.java
+++ src/main/java/enridaga/colatti/ConceptFactory.java
@@ -6,16 +6,16 @@ public interface ConceptFactory {
 
 	Concept make(Collection<Object> objects, Collection<Object> attributes);
 
-	ConceptInMemory make(Object[] objects, Object[] attributes);
+	Concept make(Object[] objects, Object[] attributes);
 
 	Concept empty();
 
-	ConceptInMemory makeFromSingleObject(Object object, Object... attributes);
+	Concept makeFromSingleObject(Object object, Object... attributes);
 
-	ConceptInMemory makeAddAttributes(Concept concept, Object... attributesToAdd);
+	Concept makeAddAttributes(Concept concept, Object... attributesToAdd);
 
-	ConceptInMemory makeAddObject(Concept concept, Object object);
+	Concept makeAddObject(Concept concept, Object object);
 
-	ConceptInMemory makeJoinAttributes(Concept concept, Object[]... attributeSetsToJoin);
+	Concept makeJoinAttributes(Concept concept, Object[]... attributeSetsToJoin);
 
 }
